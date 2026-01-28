b89a7b3651b0b43e2f9038e03a5fb3f90355e4ab
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-28 17:27:59 +0700

    add addUser function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function greet() {
+    console.log("Hello world");
+  }
+
+  function addUser() {
+    console.log("User added!");
+  }
+
+  greet();
+  addUser();
+
   return (
     <div className="App">
       <header className="App-header">
